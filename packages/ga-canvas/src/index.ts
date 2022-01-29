69cdd233d41f039b44a2927110c408c0a6142835
Alex <17277800+alex-schaaf@users.noreply.github.com>
2022-01-29 13:03:52 +0100

    🔧 fix build system using snowpack

--- packages/ga-canvas/src/index.ts
+++ packages/ga-canvas/src/index.ts
@@ -1,8 +1,1 @@
-export function createCanvas(): {
-  canvas: HTMLCanvasElement;
-  context: CanvasRenderingContext2D | null;
-} {
-  const canvas = document.createElement("canvas");
-  const context = canvas.getContext("2d");
-  return { canvas, context };
-}
+export { createCanvas } from "./utils";
